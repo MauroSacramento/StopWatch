1a7fde97bd328ce5a263dd9325c88bc4f906fa2f
Maurosacramento <maurosacramento35@gmail.com>
2024-12-30 09:42:33 +0000

    Criação dos métodos

--- script.js
+++ script.js
@@ -20,10 +20,51 @@ stopButton.addEventListener('click', stopTimer);
 pauseButton.addEventListener('click', pauseTimer);
 resetButton.addEventListener('click', resetTimer);
 
-function startTimer(){}
+function startTimer(){
+    interval = setInterval(updateTimer, 10);
+    startButton.disabled = true;
+}
 
 function stopTimer(){}
 
-function pauseTimer(){}
+function pauseTimer(){
+    clearInterval(interval);
+    stopButton.disabled = true;
+    startButton.disabled = false;
+}
 
-function resetTimer(){}
+function resetTimer(){
+
+    clearInterval(interval);
+    miliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    startButton.disabled = false;
+
+    displayTimer();
+}
+
+function updateTimer(){
+    miliseconds++;
+
+    if(miliseconds === 100){
+        miliseconds = 0;
+        seconds++;
+        if(seconds === 60){
+            seconds = 0;
+            minutes++
+        }
+    }
+
+    displayTimer()
+}
+
+function displayTimer(){
+    millisecondLabel.textContent = padTime(miliseconds);
+    secondLabel.textContent = padTime(seconds);
+    minuteLabel.textContent = padTime(minutes);
+}
+
+function padTime(time){
+    return time.toString().padStart(2, '0');
+}
